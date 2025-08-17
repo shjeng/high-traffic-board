33598a57ccedbf6f3999691349ac631a791eff4f
shjeng <ghsmsl20@gmail.com>
2025-08-17 17:43:38 +0900

    40. 조회수 어뷰징 방지 정책 구현
    - 10분마다 1개씩 증가하도록
    https://www.inflearn.com/courses/lecture?courseId=334365&tab=curriculum&type=LECTURE&unitId=258369&subtitleLanguage=ko

--- service/view/src/main/java/high_traffic_board/view/service/ArticleViewService.java
+++ service/view/src/main/java/high_traffic_board/view/service/ArticleViewService.java
@@ -1,17 +1,27 @@
 package high_traffic_board.view.service;
 
 import high_traffic_board.view.repository.ArticleViewCountRepository;
+import high_traffic_board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            // 이미 조회수가 증가돼있는 상태면 그냥 바로 조회수를 반환해줌
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
